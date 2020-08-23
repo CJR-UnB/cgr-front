b752576dd6acd2fc6f82c0215dd7955cc01939f6
nataly_lacerda <nataly.oliveira@cjr.org.br.com>
2020-08-22 22:29:06 -0300

    ✨ Atualiza rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Profile from './pages/Profile';
+import RegisterMember from './pages/Register/Member';
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path='/' component={Profile} exact />
+        <Route path='/register/member' component={RegisterMember}/>
       </Switch>
     </BrowserRouter>
   );
