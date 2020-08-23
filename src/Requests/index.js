88f4a888b3dd644210ac49412c9290c66d2c88ac
nataly_lacerda <nataly.oliveira@cjr.org.br.com>
2020-08-23 17:35:24 -0300

    🐛 Atualiza requisições

--- src/Requests/index.js
+++ src/Requests/index.js
@@ -2,8 +2,8 @@ import api from '../Api'
 
 // export const getMember = (id) => api.get(`/?page=${id}`);
 
-export const getMembers = () => api.get(`/members/1`);
+export const getMembers = () => api.get(`/members`);
 
-export const postMembers = (data) => api.post(`/members/1`, data);
+export const postMembers = (data) => api.post(`/members`, data);
 
-export const deleteMembers = (id) => api.delete(`/members/1/${id}`);
+export const deleteMembers = (id) => api.delete(`/members/${id}`);
