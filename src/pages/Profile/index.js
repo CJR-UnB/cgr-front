09a6953f4f7fde3ed2530101a2f2110b1838957e
nataly_lacerda <nataly.oliveira@cjr.org.br.com>
2020-08-29 19:16:13 -0300

    ⚡️ Mostra equipes de cada membro e adiciona ícones

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,13 +1,18 @@
 import React, { useEffect, useState } from 'react'
-import { getMembers } from '../../Requests'
-import { Page, Content, Header } from './styles'
+import { getMembers, deleteMembers } from '../../Requests'
+import { Page, Content } from './styles'
 import Sidebar from '../../components/Sidebar'
 import Accordion from 'react-bootstrap/Accordion'
 import { Card } from 'react-bootstrap'
+import { FaRegTrashAlt } from "react-icons/fa";
+import { BsFillPlusCircleFill } from "react-icons/bs";
+import { useHistory } from 'react-router-dom'
 
 export default function Profile(){
 
     const [members, setMembers] = useState([])
+
+    let history = useHistory()
 
     useEffect(() => {
         getMembers().then((res) => {
@@ -16,23 +21,36 @@ export default function Profile(){
         })
     },[])
 
+    // function handleDelete(id){
+    //     deleteMembers(id).then((res) => {
+    //         setMembers(res.data)
+    //     })
+    //     .catch(err => {
+    //         alert('buuua')
+    //     })
+    // }
+
     return(
         <Page>
             <Sidebar/>
             <Content>
-                <Header/>
-                <Accordion>
+                <Accordion className={'list'}>
                 {members.map((member) => (                    
                     <Card key={member.id}>
-                        <Accordion.Toggle as={Card.Header} eventKey={member.id}>
-                        {member.name}
+                        <Accordion.Toggle className={'cardHeader'} as={Card.Header} eventKey={member.id}>
+                            {member.name}
+                            <FaRegTrashAlt //onClick={() => handleDelete(member.id)} 
+                            />
                         </Accordion.Toggle>
                         <Accordion.Collapse eventKey={member.id}>
-                        <Card.Body>Hello! I'm the body</Card.Body>
+                            <Card.Body>Equipe(s): {member.teams.map((team) => (
+                                <p>{team.name}</p>
+                            ))}</Card.Body>
                         </Accordion.Collapse>
                     </Card>
                     ))}
                 </Accordion>
+                <BsFillPlusCircleFill className={'icon'} onClick={() => history.push('/register/member')}/>
             </Content>
         </Page>
     )
