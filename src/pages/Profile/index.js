814da0bb841a740a534580c3374523e34dabaf59
nataly_lacerda <nataly.oliveira@cjr.org.br.com>
2020-08-29 19:28:24 -0300

    🚨 Remove warning

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { getMembers, deleteMembers } from '../../Requests'
+import { getMembers } from '../../Requests'
 import { Page, Content } from './styles'
 import Sidebar from '../../components/Sidebar'
 import Accordion from 'react-bootstrap/Accordion'
