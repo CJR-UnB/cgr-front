779e54993c38e3e6b3d5653c122260d873d54d77
nataly_lacerda <nataly.oliveira@cjr.org.br.com>
2020-08-30 16:31:26 -0300

    💄 Deixa a página de Criar Membro mais decente

--- src/pages/Register/Member/index.js
+++ src/pages/Register/Member/index.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { Page } from './styles'
+import { Page, Content } from './styles'
 import { postMembers, getMembers, getTeams, getTeam } from '../../../Requests'
+import ReturnButton from '../../../components/ReturnButton'
 
 export default function RegisterMember(){
-    const [newMembers, setNewMembers] = useState({
+    const [newMember, setNewMember] = useState({
         member: {
             name: null,
         },
@@ -23,11 +24,12 @@ export default function RegisterMember(){
 
     function createMember(e){
         e.preventDefault();
-        postMembers(newMembers).then((res) => {
+        postMembers(newMember).then((res) => {
             console.log('uhul')
+            alert('Membro criado com sucesso')
         })
         .catch(err => {
-            console.log(err.response, newMembers)
+            console.log(err.response, newMember)
         })
     }
 
@@ -42,7 +44,7 @@ export default function RegisterMember(){
 
     function handleRole(e){
         const selectedIndex = e.target.options.selectedIndex
-        setNewMembers({...newMembers, role_id:e.target.options[selectedIndex].getAttribute('data-key')})
+        setNewMember({...newMember, role_id:e.target.options[selectedIndex].getAttribute('data-key')})
     }
 
     useEffect(() => {
@@ -61,36 +63,40 @@ export default function RegisterMember(){
 
     return(
         <Page>
-            <form onSubmit={createMember}>
-                <label>Name:</label><br/>
-                <input type="text" value={newMembers.member.name} onChange={e => setNewMembers({...newMembers, member: {name:e.target.value}})}/><br/>
-                
-                <label>Equipe:</label><br/>
-                <select onChange={e => handleTeam(e)}>
-                    <option value=''>Selecione uma Equipe</option>
-                    {teams.map((team) => (
-                        <option value={team.name} key={team.id} data-key={team.id} >{team.name}</option>
-                    ))}
-                </select><br/>
-                
-                <label>Cargo:</label><br/>
-                <select onChange={e => handleRole(e)}>
-                    <option value=''>Selecione um Cargo</option>
-                    {selectedTeam.roles.map((role) => (
-                        <option key={role.id} data-key={role.id} >{role.name}</option>
-                    ))}
-                </select><br/><br/>
-                
-                <input type="submit"/>
-            </form>
-            <div>
-                {members.name}
-                <ul>
-                {members.map((member) => (
-                    <li key={member.id}>{member.name}</li>
-                ))}
-                </ul>
-            </div>
+            <Content>
+            <ReturnButton/>
+                <h2>Novo Membro</h2>
+                <form onSubmit={createMember}>
+                    <label>Nome:</label><br/>
+                    <input type="text" value={newMember.member.name} onChange={e => setNewMember({...newMember, member: {name:e.target.value}})}/><br/>
+                    
+                    <label>Equipe:</label><br/>
+                    <select onChange={e => handleTeam(e)}>
+                        <option value=''>Selecione uma Equipe</option>
+                        {teams.map((team) => (
+                            <option value={team.name} key={team.id} data-key={team.id} >{team.name}</option>
+                            ))}
+                    </select><br/>
+                    
+                    <label>Cargo:</label><br/>
+                    <select onChange={e => handleRole(e)}>
+                        <option value=''>Selecione um Cargo</option>
+                        {selectedTeam.roles.map((role) => (
+                            <option key={role.id} data-key={role.id} >{role.name}</option>
+                        ))}
+                    </select><br/><br/>
+                    
+                    <input type="submit"/>
+                </form>
+                <div>
+                    {members.name}
+                    <ul>
+                    {members.map((member) => (
+                        <li key={member.id}>{member.name}</li>
+                        ))}
+                    </ul>
+                </div>
+            </Content>
         </Page>
     )
 }
--- src/pages/Register/Member/styles.js
+++ src/pages/Register/Member/styles.js
@@ -2,5 +2,12 @@ import styled from 'styled-components'
 
 export const Page = styled.div `
     background-color: #F3F4F7;
+    color: #454555;
     min-height: 100vh;
+    box-sizing: border-box;
+    padding: 5% 15%;
+`
+export const Content = styled.div `
+    width: 100%;
+    min-height: 100%;
 `
