3ee20d5a86c971f453a16aeef207b1a128a48cc9
nataly_lacerda <nataly.oliveira@cjr.org.br.com>
2020-09-04 20:07:43 -0300

    ✨ Cria componente NavButtons

---
+++ src/components/NavButtons/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { TabsContainer, Tab } from './styles'
+
+export default function NavButtons(){
+    return(
+        <TabsContainer>
+            <Tab>Equipes</Tab>
+            <Tab>Membros</Tab>
+        </TabsContainer>
+    )
+}
---
+++ src/components/NavButtons/styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const TabsContainer = styled.div `
+    min-width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+export const Tab = styled.div `
+    font-weight: 500;
+    margin: 0 2%;
+`
