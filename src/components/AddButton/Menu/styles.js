666c1734581e0fc5172e20a72584ca27cc0b0f99
nataly_lacerda <nataly.oliveira@cjr.org.br.com>
2020-09-04 20:25:19 -0300

    💄 Ajusta estilização do Menu do AddButton

--- src/components/AddButton/Menu/styles.js
+++ src/components/AddButton/Menu/styles.js
@@ -5,15 +5,16 @@ export const Container = styled.div `
     width: auto;
     
     position: absolute;
-    bottom: 15%;
+    bottom: 13%;
     right: 5%;
 `
 export const Option = styled.div `
     background-color: white;
-    color: grey;
-    box-shadow: 0px 0px 10px rgba(45, 45, 45, 0.3);
+    color: #454555;
+    border-radius: 5px;
+    box-shadow: 0px 0px 15px rgba(45, 45, 45, 0.075);
     margin: 10px 0;
-    padding: 3px 5px;;
+    padding: 5px 8px;;
 
     cursor: pointer;
 `
