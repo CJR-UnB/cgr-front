d233e5e465d5425a80cceea20a35b40dd5f2904f
nataly_lacerda <nataly.oliveira@cjr.org.br.com>
2020-09-06 20:46:04 -0300

    ⚡️ Atualiza requisições (pag de login)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MembersIndex from './pages/MembersIndex';
 import RegisterMember from './pages/Register/Member';
+import Login from './pages/Login';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,6 +12,7 @@ function App() {
       <Switch>
         <Route path='/' component={MembersIndex} exact />
         <Route path='/register/member' component={RegisterMember}/>
+        <Route path='/login' component={Login}/>
       </Switch>
     </BrowserRouter>
   );
