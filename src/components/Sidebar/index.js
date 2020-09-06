bcfe9ac3edf6b0b4abb540e7fac3000b9899e984
nataly_lacerda <nataly.oliveira@cjr.org.br.com>
2020-09-06 20:47:02 -0300

    ⚡️ Implementa hidden sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Logo from '../../assets/CjrLogo.png'
-import { Container, Menu, Item } from './styles'
+import { GoPencil } from 'react-icons/go'
+import { Container, ContainerShadow, Menu, Item, ArrowLeft, HiddenMenu, HiddenItem, ArrowRight } from './styles'
 
 export default function Sidebar(){
-    return(
-        <Container>
-            <img src={Logo} alt={'Logo CJR'} />
-            <Menu>
-                <Item>Dashboard</Item>
-                <Item>Eventos</Item>
-                <Item>Reuniões</Item>
-                <Item>Horas</Item>
-                <Item>Equipes</Item>
-            </Menu>
-        </Container>
-    )
+    const [showSidebar, setShowSidebar] = useState(true)
+
+    if (showSidebar){
+        return(
+            <>
+            <Container>
+                <img src={Logo} alt={'Logo CJR'} />
+                <Menu>
+                    <Item>Dashboard</Item>
+                    <Item>Eventos</Item>
+                    <Item>Reuniões</Item>
+                    <Item>Horas</Item>
+                    <Item>Equipes</Item>
+                </Menu>
+                <ArrowLeft onClick={() => setShowSidebar(showSidebar => !showSidebar)}/>
+            </Container>
+            <ContainerShadow/>
+            </>
+        )        
+    }
+    else{
+        return(
+            <>
+            <Container style={{width: '4vw', padding: '5% 5px'}}>
+                <img src={Logo} alt={'Logo CJR'} />
+                <HiddenMenu>
+                    <HiddenItem><GoPencil/></HiddenItem>
+                </HiddenMenu>
+                <ArrowRight onClick={() => setShowSidebar(showSidebar => !showSidebar)}/>
+            </Container>
+            <ContainerShadow style={{width: '4vw'}}/>
+            </>
+        )
+    }
 }
--- src/components/Sidebar/styles.js
+++ src/components/Sidebar/styles.js
@@ -1,25 +1,64 @@
 import styled from 'styled-components'
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 
 export const Container = styled.div `
     background-color: white;
+    color: #878D97;
+    box-shadow: 0px 0px 15px rgba(45, 45, 45, 0.075);
     height: 100vh;
     width: 20vw;
-    position: fixed;
     padding: 5%;
+    position: fixed;
 
     display: flex;
     flex-direction: column;
     align-items: center;
+    /* justify-content: space-between; */
     box-sizing: border-box;
+
+    transition: 0.4s ease;
 
     img {
         width: 90%;
     }
 `
+export const ContainerShadow = styled.div `
+    width: 20vw;
+    height: 100vh;
+
+    transition: 0.4s ease;
+`
 export const Menu = styled.div `
     display: flex;
     flex-direction: column;
+    justify-self: center;
 `
 export const Item = styled.div `
     padding: 5px 0;
+`
+export const ArrowLeft = styled(FaChevronLeft) `
+    font-size: 1.5rem;
+    color: #369FFF;
+    position: absolute;
+    right: 30px;
+    bottom: 10%;
+    cursor: pointer;
+`
+
+// Hidden Sidebar
+
+export const HiddenMenu = styled.div `
+    display: flex;
+    flex-direction: column;
+    justify-self: center;
+`
+export const HiddenItem = styled.div `
+    padding: 5px 0;
+`
+export const ArrowRight = styled(FaChevronRight) `
+    font-size: 1.5rem;
+    color: #369FFF;
+    position: absolute;
+    bottom: 10%;
+    cursor: pointer;
 `
