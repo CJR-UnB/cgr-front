707cf4a0bb5ff8c9912fa7379038012b7b1d29c9
Pedro Henrique dos Santos <pedro.santos@cjr.org.br>
2021-09-06 11:51:18 -0300

    ✨ Passa o requests e api para typescript

---
+++ src/services/requests.ts
@@ -0,0 +1,13 @@
+import api from './api';
+
+// export const getMember = (id) => api.get(`/?page=${id}`);
+
+export const getAllMembers = () => api.get('/members');
+
+export const createMember = (data: IMemberData) => api.post('/members', data);
+
+export const deleteMember = (id: number) => api.delete(`/members/${id}/unhas`);
+
+export const getAllTeams = () => api.get('/teams');
+
+export const getTeam = (id: number) => api.get(`/teams/${id}`);
