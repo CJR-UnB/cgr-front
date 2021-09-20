649ddfbe7215be5938eb8cbd3606fd9c26ff46b8
João Pedro <joao21731@gmail.com>
2021-09-20 18:56:29 -0300

    ✨ Adiciona context para autorização de login.

--- src/services/requests.ts
+++ src/services/requests.ts
@@ -1,15 +1,25 @@
 import api from './api';
+/* USERS */
 
-// export const getMember = (id) => api.get(`/?page=${id}`);
+/* MEMBER */
+export const createMember = (data: IMemberData) => api.post('/members', data);
+
+export const getMember = (id: number) => api.get(`/?page=${id}`);
 
 export const getAllMembers = () => api.get('/members');
 
-export const createMember = (data: IMemberData) => api.post('/members', data);
-
-export const postLogin = (data: { email: string; password: string; }) => api.post(`/authenticate`, data);
-
 export const deleteMember = (id: number) => api.delete(`/members/${id}/unhas`);
 
+/* TEAMS */
 export const getAllTeams = () => api.get('/teams');
 
 export const getTeam = (id: number) => api.get(`/teams/${id}`);
+
+/* LOGIN */
+export const postLogin = (data: { email: string; password: string; }) => api.post(`/authenticate`, data);
+
+export const loginUser = (email: string, password: string) =>
+    api.post('/authenticate/login', JSON.stringify({ email, password }));
+
+export const loginByToken = ( token: string ) =>
+    api.post('/authenticate/relogin', JSON.stringify({ token }));
